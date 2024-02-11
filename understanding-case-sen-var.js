3eb8a298b49f7589641172f42274f37defba00ef
Filipp Sam <philippsm666@gmail.com>
2024-02-11 20:47:09 +0300

    add understanding-case-sen-var.js

---
+++ understanding-case-sen-var.js
@@ -0,0 +1,13 @@
+/* DESCRIPTION
+Modify the existing declarations and assignments so their names use camelCase. */
+
+//SOLUTION
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
