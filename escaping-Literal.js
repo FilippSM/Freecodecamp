3418227b2fcb4fc72bbd2d02aa95ce40be754e1d
Filipp Sam <philippsm666@gmail.com>
2024-09-11 22:40:17 +0300

    add 5 tasks - 5 HTML+CSS

---
+++ escaping-Literal.js
@@ -0,0 +1,6 @@
+/* DESCRIPTION
+Use backslashes to assign a string to the myStr variable so that 
+if you were to print it to the console, you would see: */
+
+//SOLUTION
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
